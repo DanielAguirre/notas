8ff14b93894588b2bf1b948c069cf97b901029b6
DanielAguirre <danter1@gmail.com>
2015-02-02 16:15:35 -0600

    Se agrego el archivo index.js  y se configuro para empezar un servidor de expre y uno de mongoose

---
+++ index.js
@@ -0,0 +1,19 @@
+var http = require('http'),
+	mongoose = require('mongoose'),
+	conf = require('./conf'),
+	expressServer = require('./app/expressServer');
+
+mongoose.conect('mongodb://'+conf.mongoDB.host+'/'++conf.mongoDB.name);
+
+var app = new expressServer();
+
+var server = new http.createServer(app.expressServer);
+
+
+if(!module.parent){
+	server.listen(conf.port,function(){
+		console.log('runnig app');
+	});
+}else {
+	module.exports = server;
+}
