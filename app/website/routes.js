98112536d83227ced2b5f1901d1887d9427df756
DanielAguirre <danter1@gmail.com>
2015-02-02 17:16:40 -0600

    Se creo el archivo urls que contendra las urls de la aplicacion y el archivo routes en app/website manejara las rutas solo del website

---
+++ app/website/routes.js
@@ -0,0 +1,8 @@
+var express = require('express');
+	HomeController = require('./controllers/homecontroller'),
+
+var router = express.Router();
+
+router.get('',HomeController.index)
+
+module.exports = router
