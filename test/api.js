5f33bdc0fd32c66dde78942590f3c6488553571f
DanielAguirre <danter1@gmail.com>
2015-02-02 19:44:56 -0600

    Se creo la prueba get para obtener una nota existente

--- test/api.js
+++ test/api.js
@@ -46,4 +46,34 @@ describe('recurso /notas',function(){
 				});
 		})
 	})
+	describe('GET', function(){
+		it('deberia obtener una nota existente',function(done){
+			var id;
+			request
+				.post(url)
+				// Acept application/json
+				.set('Accept', 'application/json')
+				.send(data)
+				// Status Code = 201
+				.expect(201)
+				.then(function(res){
+					console.log("nota",res.body.nota._id)
+					id= res.body.nota._id
+					return request
+							.get(url+'/'+id)
+							.expect(200)
+							.expect('Content-Type',/application\/json/)
+				},done)
+				.then(function(res){
+					var nota = res.body.nota;
+					//Propiedades					
+					expect(nota).to.have.property('title', "Mejorando.la #node-pro");
+					expect(nota).to.have.property("description", "Introducion a clase");
+					expect(nota).to.have.property('type', 'js');
+					expect(nota).to.have.property('body','soy el cuerpo del json');
+					expect(nota).to.have.property('_id',id);
+					done();
+				}, done)
+		})
+	})
 })
