a154f92018b0b24da1097ae836d890fa4faa2237
DanielAguirre <danter1@gmail.com>
2015-02-02 18:37:32 -0600

    Se creo la primera prueba  el metdo post del api

--- test/api.js
+++ test/api.js
@@ -2,12 +2,45 @@
 
 var request = require('supertest-as-promised'),
 	api = require('../index'),
-	host = process.env.API_TEST_HOST || api;
+	host = process.env.API_TEST_HOST || api,
+	url='/api/notas';
+
+var data = {
+		'nota':{
+			'title': "Mejorando.la #node-pro",
+			"description": "Introducion a clase",
+			"type":"js",
+			"body": "soy el cuerpo del json"
+		}
+	};
 
 request = request(host);
 
 describe('recurso /notas',function(){
-	it('deberia crear una nueva nota', function(done){
-
+	describe('POST',function(){
+		it('deberia crear una nueva nota', function(done){
+			request.post(url)
+				.set('Accept', 'application/json')
+				.send(data)
+				.expect('Content-Type',/application\/json/)
+				.expect(201)
+				.then(function(res){
+					id = res.body.nota.id;
+					return request
+								.get('/api/notas/'+id)
+								.expect(200)
+								.expect('Content-Type',/application\/json/)
+				} ,done)
+				.then(function(res){
+					var nota = res.body.nota;
+					//Propiedades					
+					expect(nota).to.have.property('title', "Mejorando.la #node-pro");
+					expect(nota).to.have.property("description", "Introducion a clase");
+					expect(nota).to.have.property('type', 'js');
+					expect(nota).to.have.property('body','soy el cuerpo del json');
+					expect(nota).to.have.property('id',id);
+					done();
+				},done);
+		})
 	})
 })
