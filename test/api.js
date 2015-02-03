47d5f06ec06df64c8069d8458534719e7154332a
DanielAguirre <danter1@gmail.com>
2015-02-02 21:12:18 -0600

    Se creo la prueba Put para actualizar una nota existente

--- test/api.js
+++ test/api.js
@@ -57,7 +57,6 @@ describe('recurso /notas',function(){
 				// Status Code = 201
 				.expect(201)
 				.then(function(res){
-					console.log("nota",res.body.nota._id)
 					id= res.body.nota._id
 					return request
 							.get(url+'/'+id)
@@ -74,6 +73,40 @@ describe('recurso /notas',function(){
 					expect(nota).to.have.property('_id',id);
 					done();
 				}, done)
+		})
+	})
+	describe('PUT', function(){
+		it('deberia actualizar una nota existente', function(done){
+			var id;
+			request.post(url)
+				.set('Accept', 'application/json')
+				.send(data)
+				.expect(201)
+				.then(function(res){
+					id= res.body.nota._id
+					return request.get('/api/notas/'+id)
+							.set('Accept', 'application/json')
+							.send()
+				}, done)
+				.then(function(res){
+					var nota = res.body.nota;
+
+					nota.title = 'Juan Pablo no Sabe escribir ejecucción';
+					return request
+							.put(url+'/'+id)
+							.expect(200)
+							.expect('Content-Type',/application\/json/)
+				},done)
+				.then(function(res){
+					var nota = res.body.nota;					
+					//Propiedades					
+					expect(nota).to.have.property('title', "Juan Pablo no Sabe escribir ejecucción");
+					expect(nota).to.have.property("description", "Introducion a clase");
+					expect(nota).to.have.property('type', 'js');
+					expect(nota).to.have.property('body','soy el cuerpo del json');
+					expect(nota).to.have.property('id',id);
+					done();
+				},done)
 		})
 	})
 })
