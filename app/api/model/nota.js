8f579863421060df3c961681b15d9c205b3502f8
DanielAguirre <danter1@gmail.com>
2015-02-02 19:19:49 -0600

    SE creo el metodo  para guardar notas en el modelo notas.js

---
+++ app/api/model/nota.js
@@ -0,0 +1,11 @@
+var modelNota  =require('./schema/schema');
+
+var Nota = function(){}
+
+Nota.save = function(data, callback){
+	modelNota
+		.create(data,function(err,data){
+			if(err) throw err;
+			callback(data);
+		})
+}
